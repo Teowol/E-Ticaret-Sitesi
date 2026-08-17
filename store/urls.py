5804d42f3eafb83ba4b20f7c354be5bdd972f4e1
Teoman Ünal <teomanunal06@gmail.com>
2026-08-17 08:56:44 +0000

    docker kuruldu

--- store/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('urun/<slug:slug>/', views.product_detail, name='product_detail'),
-]
