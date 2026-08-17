e0ee68f78667ae17c97b1ac1960537990d4af7ab
Teoman Ünal <teomanunal06@gmail.com>
2026-08-17 13:18:59 +0000

    TemaEklendi+DosyalarDüzenlendi

---
+++ static/js/theme.js
@@ -0,0 +1,36 @@
+(function () {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    document.documentElement.setAttribute("data-theme", savedTheme);
+})();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const themeButton = document.getElementById("themeToggle");
+
+    function updateThemeButton() {
+        const theme = document.documentElement.getAttribute("data-theme");
+        const icon = themeButton.querySelector("i");
+        const text = themeButton.querySelector("span");
+
+        if (theme === "dark") {
+            icon.className = "bi bi-sun-fill";
+            text.textContent = " Light";
+        } else {
+            icon.className = "bi bi-moon-fill";
+            text.textContent = " Dark";
+        }
+    }
+
+    updateThemeButton();
+
+    themeButton.addEventListener("click", function () {
+        const currentTheme =
+            document.documentElement.getAttribute("data-theme");
+
+        const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+        document.documentElement.setAttribute("data-theme", newTheme);
+        localStorage.setItem("theme", newTheme);
+
+        updateThemeButton();
+    });
+});
